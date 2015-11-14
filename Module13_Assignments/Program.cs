a51200b79e64e37c7af140e6cfc1679330eba4c8
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-11-14 11:12:09 -0600

    First Commit

---
+++ Module13_Assignments/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13_Assignments
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            byte b = 127;
+            char c = 'A';
+            short s = 23;
+            int i = 1;
+            long l = 53;
+            float f = 23.34f;
+            double d = 23.42;
+            bool bl = true;
+            string str = "Hello World (from the string variable).";
+
+
+            i = (int)b + 1;
+
+            double d2 = 23.34;
+            int i2 = 23;
+
+            int[] ary = { 41, 52, 73, 84, 95 };
+            int counterWhileLoop = ary.Length;
+            int j = 0;
+            while (j < counterWhileLoop)
+            {
+                Console.WriteLine(ary[j]);
+                j++;
+            }
+
+            Console.WriteLine();
+
+            for (int x = 0; x < ary.Length; ++x)
+            {
+                Console.WriteLine(ary[x]);
+            }
+
+            Console.WriteLine(str + " Value of byte is = {0}.", b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module13_Assignments/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Module13_Assignments")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Module13_Assignments")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e5630c72-4c44-4386-8a3d-4d279f3bd204")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
