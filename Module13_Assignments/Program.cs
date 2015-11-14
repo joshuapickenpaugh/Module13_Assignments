009da499abbd71b1bdcc9a86992c7d48a06ef94c
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-11-14 12:54:24 -0600

    Last Commit

--- Module13_Assignments/Program.cs
+++ Module13_Assignments/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Module13_Assignments
 {
+    //1a:
     class Class1
     {
         static void Main(string[] args)
         {
+            //1b:
             byte b = 127;
             char c = 'A';
             short s = 23;
@@ -20,13 +22,20 @@ namespace Module13_Assignments
             bool bl = true;
             string str = "Hello World (from the string variable).";
 
+            Console.WriteLine(str + " Value of byte is = {0}.", b);
 
-            i = (int)b + 1;
-
-            double d2 = 23.34;
-            int i2 = 23;
-
+            //1c:
             int[] ary = { 41, 52, 73, 84, 95 };
+
+            Console.WriteLine("ForEach loop:");
+            foreach (int number in ary)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("While loop:");
             int counterWhileLoop = ary.Length;
             int j = 0;
             while (j < counterWhileLoop)
@@ -37,13 +46,48 @@ namespace Module13_Assignments
 
             Console.WriteLine();
 
+            Console.WriteLine("For loop:");
             for (int x = 0; x < ary.Length; ++x)
             {
                 Console.WriteLine(ary[x]);
             }
 
-            Console.WriteLine(str + " Value of byte is = {0}.", b);
+            Console.WriteLine();
+
+            Console.WriteLine("Do loop:");
+            int t = 0;
+            do
+            {
+                Console.WriteLine(ary[t]);
+                t++;
+            }
+            while (t < ary.Length);
+
+            Console.WriteLine();
+
+            //1d:
+            Console.WriteLine("Press any key EXCEPT 'enter':");
+            if (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("Good Job!");
+            }
+            else
+            {
+                Console.WriteLine("You pressed the ENTER key!");
+            }
+
+            //1e:
+            AddNumbers(5, 7);
+
+            Console.WriteLine("Press any key to EXIT.");
             Console.ReadKey();
+        }
+
+        static void AddNumbers(int x, int y)
+        {
+            int total = 0;
+            total = x + y;
+            Console.WriteLine(total);
         }
     }
 }
